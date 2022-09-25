b461887ae5148c1a31a70b599f575c1e0a41559e
wangbo <wangbo>
2022-09-25 22:14:04 +0800

    java config配置Bean

---
+++ spring-pritice/src/main/java/com/scarecrow/spring/pritice/config/UserBeanConfig.java
@@ -0,0 +1,18 @@
+package com.scarecrow.spring.pritice.config;
+
+import com.scarecrow.spring.pritice.life.UserBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UserBeanConfig {
+
+	@Bean
+	public UserBean userBean() {
+		UserBean userBean = new UserBean();
+		userBean.setUsername("java config");
+		userBean.setPwd("java config");
+		userBean.setTel("java config");
+		return userBean;
+	}
+}
---
+++ spring-pritice/src/main/java/com/scarecrow/spring/pritice/config/UserBeanConfigTest.java
@@ -0,0 +1,14 @@
+package com.scarecrow.spring.pritice.config;
+
+import com.scarecrow.spring.pritice.life.UserBean;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class UserBeanConfigTest {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(UserBeanConfig.class);
+		UserBean userBean = applicationContext.getBean(UserBean.class);
+		System.out.println(userBean);
+		applicationContext.close();
+	}
+}
